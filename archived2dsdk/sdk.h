97a8d045a75f80200621bfe9eeff1234c2ac2aca
Matthew Polk <183302572+xrheihwh@users.noreply.github.com>
2024-10-08 06:57:31 -0400

    Update

---
+++ archived2dsdk/sdk.h
@@ -0,0 +1,10 @@
+/*
+Copyright (C) 2024, Matthew Polk.
+This file is free software licensed under the GPLv3. See COPYING for details.
+
+There are two parts to the toonz sdk.
+1. The low level COM like interface.
+2. A wrapper, should you make one.
+
+This header file concerns the wrapper.
+*/
