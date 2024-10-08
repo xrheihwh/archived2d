db9c564934c97db06efd454a81811c7f1e30a7d6
Matthew Polk <183302572+xrheihwh@users.noreply.github.com>
2024-10-08 07:08:18 -0400

    Update

--- archived2dsdk/sdk.h
+++ archived2dsdk/sdk.h
@@ -7,4 +7,13 @@ There are two parts to the toonz sdk.
 2. A wrapper, should you make one.
 
 This header file concerns the wrapper.
+toonz_utility.hpp is the wrapper and toonz_hostif.h, toonz_params.h, and toonz_plugin.h
+is the low level SDK. What the wrapper does is the following:
+1. Write out the proper steps to create and register a plugin easily.
+2. Any additional functionality the plugins should be using.
+
+
+
+
+
 */
